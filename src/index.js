1e4d4ddb0ccca51f818f5b720faadaafdaec95ac
Attila Bagossy <bagossyattila@outlook.com>
2020-04-02 22:26:40 +0200

    Added the husky and standard dependencies along with appropriate script and hooks

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
 const yargs = require('yargs')
 
-function cli() {
-    return yargs
-        .command('$0 <artifact> [arguments...]', '', yargs => {
-            yargs
-                .positional('artifact', {
-                    describe: '',
-                    type: 'string'
-                })
-                .positional('arguments', {
-                    describe: '',
-                    type: 'string'
-                })
-                .options({
-                    'ignore-local': {
-                        describe: '',
-                        type: Boolean,
-                        conflicts: ['only-local']
-                    },
-                    'only-local': {
-                        describe: '',
-                        type: Boolean,
-                        conflicts: ['ignore-local']
-                    },
-                    'java-executable': {
-                        alias: 'j',
-                        describe: '',
-                        type: 'string'
-                    },
-                    'local-repository': {
-                        alias: 'l',
-                        describe: '',
-                        type: 'string'
-                    },
-                    'quiet': {
-                        alias: 'q',
-                        describe: '',
-                        type: Boolean
-                    },
-                })
+function cli () {
+  return yargs
+    .command('$0 <artifact> [arguments...]', '', yargs => {
+      yargs
+        .positional('artifact', {
+          describe: '',
+          type: 'string'
         })
-        .strict(true)
-        .argv
+        .positional('arguments', {
+          describe: '',
+          type: 'string'
+        })
+        .options({
+          'ignore-local': {
+            describe: '',
+            type: Boolean,
+            conflicts: ['only-local']
+          },
+          'only-local': {
+            describe: '',
+            type: Boolean,
+            conflicts: ['ignore-local']
+          },
+          'java-executable': {
+            alias: 'j',
+            describe: '',
+            type: 'string'
+          },
+          'local-repository': {
+            alias: 'l',
+            describe: '',
+            type: 'string'
+          },
+          quiet: {
+            alias: 'q',
+            describe: '',
+            type: Boolean
+          }
+        })
+    })
+    .strict(true)
+    .argv
 }
 
-function run() {
-    
+function run () {
+  console.log(cli())
 }
 
 module.exports = {
-    run
+  run
 }
