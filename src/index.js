e5fa8b5305d2ac844254d3968e2519d0600abf1d
Attila Bagossy <bagossyattila@outlook.com>
2020-04-02 22:21:54 +0200

    Removed the separated execute command module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-const mvnArtifactDownload = require('mvn-artifact-download')
 const yargs = require('yargs')
 
-function run() {
-    yargs
-        .commandDir('./commands')
-        .demandCommand()
+function cli() {
+    return yargs
+        .command('$0 <artifact> [arguments...]', '', yargs => {
+            yargs
+                .positional('artifact', {
+                    describe: '',
+                    type: 'string'
+                })
+                .positional('arguments', {
+                    describe: '',
+                    type: 'string'
+                })
+                .options({
+                    'ignore-local': {
+                        describe: '',
+                        type: Boolean,
+                        conflicts: ['only-local']
+                    },
+                    'only-local': {
+                        describe: '',
+                        type: Boolean,
+                        conflicts: ['ignore-local']
+                    },
+                    'java-executable': {
+                        alias: 'j',
+                        describe: '',
+                        type: 'string'
+                    },
+                    'local-repository': {
+                        alias: 'l',
+                        describe: '',
+                        type: 'string'
+                    },
+                    'quiet': {
+                        alias: 'q',
+                        describe: '',
+                        type: Boolean
+                    },
+                })
+        })
         .strict(true)
         .argv
+}
+
+function run() {
+    
 }
 
 module.exports = {
