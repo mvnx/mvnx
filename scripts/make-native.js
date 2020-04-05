a0cd77d75d37d4498a7ca64d96ca3983e80fce07
Attila Bagossy <bagossyattila@outlook.com>
2020-04-05 20:10:04 +0200

    Automatically pushing linux executables on tag creation

---
+++ scripts/make-native.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+const pkg = require('pkg')
+
+const { version } = require('../package')
+
+async function makeTarget (target) {
+  let output = `./native/mvnx-v${version}-${target}`
+
+  if (target.includes('win')) {
+    output += '.exe'
+  }
+
+  console.log(`Building native executable for target "${target}" at "${output}"`)
+
+  await pkg.exec(['.', '--target', target, '--output', output])
+}
+
+(async function main () {
+  const target = process.argv[2]
+
+  if (!target) {
+    console.error('Please specify a target as a command line argument!')
+
+    process.exit(1)
+  }
+
+  try {
+    await fs.promises.mkdir('native')
+  } catch {
+    // Hopefully, it just exists.
+  }
+
+  await makeTarget(target)
+})()
