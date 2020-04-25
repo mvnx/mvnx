e4401fd75089cdd18bbcc98c829f2b6e3fcafd45
Attila Bagossy <bagossyattila@outlook.com>
2020-04-25 14:29:17 +0200

    Artifacts may include an unparsed but validated repository part

--- src/lib/artifact/parser.js
+++ src/lib/artifact/parser.js
@@ -1,14 +1,49 @@
+const { URL } = require('url')
+
 const ARTIFACT_SEGMENT_SEPARATOR = ':'
 const ARTIFACT_SEGMENT_REGEX = /^[a-zA-Z0-9]([a-zA-Z0-9-_.][a-zA-Z0-9]|[a-zA-Z0-9])*$/g
 
+const REPOSITORY_IDENTIFIER_SEGMENT_REGEX = /^[a-zA-Z0-9-_.]+$/g
+
 const GROUP_ID_SEGMENT_SEPARATOR = '.'
+const URL_SEPARATOR = '/'
 
 const MIN_SEGMENT_COUNT = 2
 const MAX_SEGMENT_COUNT = 5
 
 const SNAPSHOT_SUFFIX = '-SNAPSHOT'
 
-function isArtifactName (name) {
+function nameIncludesRepository (name) {
+  return name.includes(URL_SEPARATOR)
+}
+
+function isValidUrl (repository) {
+  try {
+    // Will throw TypeError if repository is not a valid URL.
+    // Not the most elegant way to check the validity but it's okay.
+    new URL(repository) // eslint-disable-line no-new
+
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function isValidRepositoryIdentifier (repository) {
+  const segments = repository.split(URL_SEPARATOR)
+
+  return segments.every(segment => segment.match(REPOSITORY_IDENTIFIER_SEGMENT_REGEX) !== null)
+}
+
+function isArtifactName (name = '') {
+  if (nameIncludesRepository(name)) {
+    const isValidRepositoryPart = isValidUrl(name) || isValidRepositoryIdentifier(name)
+
+    if (!isValidRepositoryPart) {
+      return false
+    }
+  }
+
   const segments = name.split(ARTIFACT_SEGMENT_SEPARATOR)
 
   if (segments.length < MIN_SEGMENT_COUNT || segments.length > MAX_SEGMENT_COUNT) {
